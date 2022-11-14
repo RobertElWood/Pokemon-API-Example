35cb2e22a2ebfe27dc98bcdee866a79095dca0c5
Robert Woods <robert.woods@fulbrightmail.org>
2022-11-14 14:57:46 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PokemonService } from './pokemon.service';
+import { Pokemon } from './Pokemon';
+import { RandomNumberGenerationService } from './random-number-generation.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'APIExample';
+
+  output : number = 0;
+
+  resultPokemon :  Pokemon[] = [];
+
+  SearchName : string = "";
+  
+  SearchIndex : number = 1;
+
+  constructor(private rng : RandomNumberGenerationService, private pokeAPI : PokemonService) 
+  {
+    this.output = this.rng.GetRandomNumber(1,10);
+  }
+
+  GetRandomNum(min : number, max : number)
+  {
+    return Math.floor(Math.random() * max) + min;      
+  }
+
+  UpdateOutput()
+  {
+    return this.output = this.GetRandomNum(1, 10);
+  }
+
+  SearchPokemonByName(name: string) : void 
+  {
+    this.resultPokemon = [];
+    this.pokeAPI.GetPokemonByName(name).subscribe((result: Pokemon) => {this.resultPokemon.push(result)});
+  }
+
+  GetRandomPokemon() : number
+  {
+    return this.GetRandomNum(1, 905);
+  }
+
+  SearchPokemonByIndex(index : number) : void 
+  {
+    this.resultPokemon = [];
+    this.pokeAPI.GetPokemonByIndex(index).subscribe((result: Pokemon) => {this.resultPokemon.push(result)});
+  }
+
 }
---
+++ src/app/pokemon.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import {Pokemon} from './Pokemon';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+baseURL : string = "https://pokeapi.co/api/v2/pokemon"
+
+  constructor(private http: HttpClient) { }
+
+  GetPokemonByName(name:string) : Observable <Pokemon> 
+  {
+    return this.http.get<Pokemon>(this.baseURL + "/" + name);
+  }
+
+  GetPokemonByIndex(index : number) : Observable <Pokemon> 
+  {
+    return this.http.get<Pokemon>(this.baseURL + `-species/${index}/`);
+  }
+}
